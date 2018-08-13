3df8c6dfc0cba9c82c87ffc6295bc8b5cc3e1657
rahul <snakepowerpoint@gmail.com>
2018-08-13 15:45:44 +0800

    add install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,24 @@
+# General
+pip install --upgrade pip
+
+# For app
+pip install PyQt5
+
+# For face_detector
+pip install numpy
+pip install tensorflow
+pip install opencv-python
+pip install pyzmq
+pip install pillow
+pip install coloredlogs
+
+# For face_recognizer
+pip install scipy
+pip install sklearn
+pip install matplotlib
+pip install mpld3
+pip install jinja2
+
+# For 3rd party lib
+pip install baidu-aip
+
