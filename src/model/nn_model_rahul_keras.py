30a03ab239449525b0076fc2b1740e0398e29f15
rahul <snakepowerpoint@gmail.com>
2018-08-16 14:04:08 +0800

    modify .gitignore

--- src/model/nn_model_rahul_keras.py
+++ src/model/nn_model_rahul_keras.py
@@ -14,16 +14,16 @@ from keras.regularizers import l1_l2
 def nn_model(input_shape):
 
     X_input = Input(input_shape)
-    X = Dense(64, kernel_initializer='he_uniform', name='fc1')(X_input)
+    X = Dense(128, kernel_initializer='he_uniform', name='fc1')(X_input)
     X = Activation('relu')(X)
     X = BatchNormalization(name='bn1')(X)
-    X = Dense(64, kernel_initializer='he_uniform', name='fc2')(X)
+    X = Dense(128, kernel_initializer='he_uniform', name='fc2')(X)
     X = Activation('relu')(X)
     X = BatchNormalization(name='bn2')(X)
-    X = Dense(128, kernel_initializer='he_uniform', name='fc3')(X)
+    X = Dense(256, kernel_initializer='he_uniform', name='fc3')(X)
     X = Activation('relu')(X)
     X = BatchNormalization(name='bn3')(X)
-    X = Dense(128, kernel_initializer='he_uniform', name='fc4')(X)
+    X = Dense(256, kernel_initializer='he_uniform', name='fc4')(X)
     X = Activation('relu')(X)
     X = Dense(4, activation='softmax', kernel_initializer='he_uniform', name='output')(X)
 
