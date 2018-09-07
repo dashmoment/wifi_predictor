335e4ea0f4797daa0ae9c34b36a62c6cf33c9e5d
rahul <snakepowerpoint@gmail.com>
2018-09-07 15:36:58 +0800

    correct bugs in feature_extraction_sample_ma.py

--- src/feature_extraction/feature_extraction_sample_ma.py
+++ src/feature_extraction/feature_extraction_sample_ma.py
@@ -85,7 +85,7 @@ class feature_extraction:
         
         return interval
     
-    def get_train_test_interval(self, data_length, num_interval=4000, time_step=15):
+    def get_train_test_interval(self, data_length, num_interval, time_step):
 
         # === a function to sample training and testing interval with data length given
         maxInt = data_length//time_step - 1
@@ -98,23 +98,27 @@ class feature_extraction:
         else:
             training_interval = [x for x in zip(np.append(0, selectInt[:-1]+1)*time_step, selectInt*time_step)]
         training_interval.append([training_interval[-1][1], data_length])
-        
+        training_interval = [x for x in training_interval if x[1]-x[0] != 0]
+    
         return training_interval, testing_interval
 
-    def generator(self, coll_prefix, time_step = 15, special_list = [], embed_model_path = ""):
+    def generator(self, coll_prefix, time_step = 15, num_interval = 4000, special_list = [], embed_model_path = ""):
             
             # ==== Create proper format for machine learning
 
             fdata = self._getData(coll_prefix) 
             devlist = ['AP', 'STA']
             labellist = ['Rcv', 'CCK_ERRORS', 'CRC-ERR', 'FCSError', 'OFDM_ERRORS', 'SS_Rssi']
-
+            
+            training_interval, testing_interval = self.get_train_test_interval(len(fdata), num_interval=num_interval, time_step=time_step)
+            
+            ### process training data
             #init continer for dictionary to dataFrame
             ProcMLData={}
             #ProcMLData['Time'] = []
-            TimeRecord = {}
-            TimeRecord['Time'] = []
-
+            train_TimeRecord = {}
+            train_TimeRecord['Time'] = []
+            
             for dev in devlist:
                 ProcMLData['Delay-mean'] = []
                 ProcMLData['Delay-max'] = []
@@ -127,9 +131,6 @@ class feature_extraction:
                     for i in range(56): 
                         ProcMLData[dev+'-SS_Subval-mean-'+str(i)] = []
             
-            training_interval, testing_interval = self.get_train_test_interval(len(fdata), time_step=time_step)
-            
-            ### process training data
             for interval in training_interval:
                 for idx in range(interval[0], interval[1] - time_step + 1):
 
@@ -182,11 +183,11 @@ class feature_extraction:
                             else:
                                 continue
                     #ProcMLData['Time'].append(fdata[idx]['Time'])
-                    TimeRecord['Time'].append(fdata[idx]['Time'])
+                    train_TimeRecord['Time'].append(fdata[idx]['Time'])
             
             train = pd.DataFrame(ProcMLData)
-            TimeRecord = pd.DataFrame(TimeRecord)
-            train = pd.concat([TimeRecord, train], axis = 1)
+            train_TimeRecord = pd.DataFrame(train_TimeRecord)
+            train = pd.concat([train_TimeRecord, train], axis = 1)
             label_train = self.label_generator(train)
 
             #Add SS_Subval_emb: which is embeded 56 SS_Subval into N embeded signal by NN model    
@@ -198,73 +199,88 @@ class feature_extraction:
                 #spike_cols = [col for col in MLdf.columns if 'SS_Subval' in col]
                 #MLdf.drop(spike_cols, axis=1, inplace=True) 
                 
-                df = pd.concat([df, embeded_signal], axis=1)
+                train = pd.concat([train, embeded_signal], axis=1)
                             
             
             ### process testing data
+            #init continer for dictionary to dataFrame
+            ProcMLData={}
+            test_TimeRecord = {}
+            test_TimeRecord['Time'] = []
+
+            for dev in devlist:
+                ProcMLData['Delay-mean'] = []
+                ProcMLData['Delay-max'] = []
+                for label in labellist:
+                    ProcMLData[dev+'-'+label+'-mean'] = []
+                    
+                if 'SS_Subval' in special_list:
+                    #SS_Subval should have 56, if length not 56--> ignore
+                    #56 is number of feqency  
+                    for i in range(56): 
+                        ProcMLData[dev+'-SS_Subval-mean-'+str(i)] = []
+            
             for interval in testing_interval:
-                for idx in range(interval[0], interval[1] - time_step + 1):
+                isNull = False
+                current = list(fdata[interval[0]: interval[1]])  # Get current time step data
 
-                    isNull = False
-                    current = list(fdata[idx: idx + time_step])  # Get current time step data
+                SS_Subval_tmp = {}
+                features_tmp = {}
 
-                    SS_Subval_tmp = {}
-                    features_tmp = {}
+                for dev in devlist:
+                    if 'SS_Subval' in special_list:
+                        mean_SS_Subval = self._feature_extraction(
+                            current, dev, 'SS_Subval', np.mean, 0)
+                        #SS_Subval should have 56, if length not 56--> ignore
+                        if isinstance(mean_SS_Subval,  np.ndarray) and len(mean_SS_Subval) == 56:
+                            for i in range(56):
+                                SS_Subval_tmp[dev + '-SS_Subval-mean-' +
+                                              str(i)] = mean_SS_Subval[i]
+                        else:
+                            isNull = True
 
-                    for dev in devlist:
-                        if 'SS_Subval' in special_list:
-                            mean_SS_Subval = self._feature_extraction(
-                                current, dev, 'SS_Subval', np.mean, 0)
-                            #SS_Subval should have 56, if length not 56--> ignore
-                            if isinstance(mean_SS_Subval,  np.ndarray) and len(mean_SS_Subval) == 56:
-                                for i in range(56):
-                                    SS_Subval_tmp[dev + '-SS_Subval-mean-' +
-                                                  str(i)] = mean_SS_Subval[i]
-                            else:
-                                isNull = True
+                    for label in labellist:
+                        feature = self._feature_extraction(current, dev, label, np.mean)
 
-                        for label in labellist:
-                            feature = self._feature_extraction(current, dev, label, np.mean)
-
-                            if math.isnan(feature):
-                                isNull = True
-                                break
-                            else:
-                                features_tmp[dev+'-'+label+'-mean'] = feature
-
-                        if isNull:
+                        if math.isnan(feature):
+                            isNull = True
                             break
                         else:
+                            features_tmp[dev+'-'+label+'-mean'] = feature
+
+                    if isNull:
+                        break
+                    else:
+                        continue
+
+                if not isNull:
+
+                    if 'SS_Subval' in special_list:
+                        for k in SS_Subval_tmp:
+                            ProcMLData[k].append(SS_Subval_tmp[k])
+                    for k in features_tmp:
+                        ProcMLData[k].append(features_tmp[k])
+
+                    ProcMLData['Delay-mean'].append(
+                        self._feature_extraction(current, 'AP', 'Delay', np.mean))
+                    ProcMLData['Delay-max'].append(self._feature_extraction(current,
+                                                                            'AP', 'Delay', np.max))
+
+                    #********Check Length**********
+                    stadard_len = len(ProcMLData['Delay-mean'])
+                    for k in ProcMLData:
+                        #if k == 'Time':
+                        #    continue
+                        if len(ProcMLData[k]) != stadard_len:
+                            raise Exception('Length error:{},{}, {}, {}'.format(interval[0], k, stadard_len, len(ProcMLData[k])))
+                        else:
                             continue
-
-                    if not isNull:
-
-                        if 'SS_Subval' in special_list:
-                            for k in SS_Subval_tmp:
-                                ProcMLData[k].append(SS_Subval_tmp[k])
-                        for k in features_tmp:
-                            ProcMLData[k].append(features_tmp[k])
-
-                        ProcMLData['Delay-mean'].append(
-                            self._feature_extraction(current, 'AP', 'Delay', np.mean))
-                        ProcMLData['Delay-max'].append(self._feature_extraction(current,
-                                                                                'AP', 'Delay', np.max))
-
-                        #********Check Length**********
-                        stadard_len = len(ProcMLData['Delay-mean'])
-                        for k in ProcMLData:
-                            #if k == 'Time':
-                            #    continue
-                            if len(ProcMLData[k]) != stadard_len:
-                                raise Exception('Length error:{},{}, {}, {}'.format(idx, k, stadard_len, len(ProcMLData[k])))
-                            else:
-                                continue
-                    #ProcMLData['Time'].append(fdata[idx]['Time'])
-                    TimeRecord['Time'].append(fdata[idx]['Time'])
+                #ProcMLData['Time'].append(fdata[idx]['Time'])
+                test_TimeRecord['Time'].append(fdata[interval[0]]['Time'])
 
             test = pd.DataFrame(ProcMLData)
-            TimeRecord = pd.DataFrame(TimeRecord)
-            test = pd.concat([TimeRecord, test], axis=1)
+            test_TimeRecord = pd.DataFrame(test_TimeRecord)
+            test = pd.concat([test_TimeRecord, test], axis=1)
             label_test = self.label_generator(test)
 
             #Add SS_Subval_emb: which is embeded 56 SS_Subval into N embeded signal by NN model
@@ -276,7 +292,7 @@ class feature_extraction:
                 #spike_cols = [col for col in MLdf.columns if 'SS_Subval' in col]
                 #MLdf.drop(spike_cols, axis=1, inplace=True)
 
-                df = pd.concat([df, embeded_signal], axis=1)
+                test = pd.concat([test, embeded_signal], axis=1)
 
 
             return train, label_train, test, label_test
