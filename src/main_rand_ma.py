54a99ea88985d8ac6cc8d02478fc18aec84bce43
rahul <snakepowerpoint@gmail.com>
2018-09-14 11:48:47 +0800

    change file name and remove useless files

--- src/main_rand_ma.py
+++ src/main_rand_ma.py
@@ -24,6 +24,7 @@ from xgboost import plot_importance
 import keras
 import keras.backend as K
 
+from feature_extraction import split_dataset
 from feature_extraction import label_generator as label_gen
 from feature_extraction import label_generator_rahul as label_gen_r
 from feature_extraction import feature_engineering
@@ -32,67 +33,32 @@ from utility import io
 from utility import save_load_Keras
 from utility import keras_event_callBack
 
-import pickle
-import importlib
-from datetime import datetime
-
-# importlib.reload(nn_model_rahul)
-# importlib.reload(feature_extraction_rahul)
-
 ROOT_DIR = path.dirname(path.abspath(__file__))
 
-with open('../data/raw_data_w_time_sub.pkl', 'rb') as input:
 
-    train = pickle.load(input)
-    label_train = pickle.load(input)
-    test_raw = pickle.load(input)
-    label_test_raw = pickle.load(input)
+def neural_network(data="attenuator", ap_only=False, drop_error=False,
+                   num_feature=8, lr=0.0005, batch_size=64, epochs=10,
+                   weighted_loss=True):
 
-train, label_train = label_gen_r.sort_by_time(train, label_train)
-test_raw, label_test_raw = label_gen_r.sort_by_time(test_raw, label_test_raw)
+    train, label_train, test, label_test, test_raw, label_test_raw = split_dataset.split_dataset(data)
+    
+    train = feature_engineering.binding(train)
+    test = feature_engineering.binding(test)
+    test_raw = feature_engineering.binding(test_raw)
+    
+    if ap_only:  # use only features of AP
+        ap_col = [col for col in train.columns if 'AP' in col]
+        train = train[ap_col]
+        test = test[ap_col]
+        test_raw = test_raw[ap_col]
+    
+    if drop_error:  # drop features related to error counts
+        err_col = [col for col in train.columns if 'ERRORS' in col or 'ERR' in col or 'Error' in col]
+        train = train.drop(err_col, axis=1)
+        test = test.drop(err_col, axis=1)
+        test_raw = test_raw.drop(err_col, axis=1)
 
-train = feature_engineering.binding(train)
-test_raw = feature_engineering.binding(test_raw)
-
-'''
-# Use AP columns only
-ap_col = [col for col in train.columns if 'AP' in col]
-train = train[ap_col]
-test_raw = test_raw[ap_col]
-'''
-
-'''
-# Drop error columns
-err_col = [col for col in train.columns if 'ERRORS' in col or 'ERR' in col or 'Error' in col]
-train = train.drop(err_col, axis=1)
-test_raw = test_raw.drop(err_col, axis=1)
-'''
-
-
-# Use only attenuator data to split into training and testing set
-train, label_train, test, label_test = label_gen_r.random_sample_conti(train, label_train, fraction=0.8)
-
-# Use only office data to split into training and testing set
-#train, label_train, test, label_test = label_gen_r.random_sample_conti(test_raw, label_test_raw, fraction=0.8)
-
-
-
-'''
-# Combine attenuator and office data to split into training and testing set
-total = pd.concat([train, test_raw])
-label_total = pd.concat([label_train['delay_mean'], label_test_raw['delay_mean']])
-label_total = pd.DataFrame(label_total).rename(columns={'Delay-mean': 'delay_mean'})
-
-train, label_train, test, label_test = label_gen_r.random_sample_conti(total, label_total, fraction=0.8)
-'''
-
-print(train.isnull().any())
-print(train.isnull().sum())
-
-
-def neural_network(num_feature=8, lr=0.0005, batch_size=64, epochs=1):
-
-    # feature selection
+    # feature selection by regression
     if num_feature > train.shape[1]:
         num_feature = train.shape[1]
     anova_filter = SelectKBest(f_regression, k=num_feature).fit(train, label_train['delay_mean'])
@@ -106,7 +72,7 @@ def neural_network(num_feature=8, lr=0.0005, batch_size=64, epochs=1):
     test_raw_set = [test_raw.iloc[:, mask],
                     label_gen_r.transfer_to_one_hot_class(label_test_raw['delay_mean'])]
 
-    # normalization
+    # normalization on features
     train_set[0] = (train_set[0] - train_set[0].mean())/train_set[0].std()
     test_set[0] = (test_set[0] - test_set[0].mean())/test_set[0].std()
     test_raw_set[0] = (test_raw_set[0] - test_raw_set[0].mean())/test_raw_set[0].std()
@@ -114,23 +80,22 @@ def neural_network(num_feature=8, lr=0.0005, batch_size=64, epochs=1):
     # apply NN model on selected data
     ops.reset_default_graph()
 
-    wifi = nn_model(train_set[0].shape[1:])
-
+    wifi = nn_model(train_set[0].shape[1:]) # input is shape
     adam = keras.optimizers.Adam(lr=lr, epsilon=1e-8)
-    adam_decay = keras.optimizers.Adam(lr=lr, decay=1e-10, epsilon=1e-8)
-    sgd = keras.optimizers.SGD(lr=lr, momentum=0.9)
-    sgd_decay = keras.optimizers.SGD(lr=lr, momentum=0.9, decay=1e-10)
     wifi.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
     # weighted loss
-    y_train_classes = label_gen_r.transform_delay2category(label_train['delay_mean'])
-    y_train_classes = pd.DataFrame(y_train_classes, columns=['class'])
-    n = len(y_train_classes)
-    y_train_classes_count = n / y_train_classes['class'].value_counts().sort_index(axis=0)
+    if weighted_loss:
+        y_train_classes = label_gen_r.transform_delay2category(label_train['delay_mean'])
+        y_train_classes = pd.DataFrame(y_train_classes, columns=['class'])
+        n = len(y_train_classes)
+        y_train_classes_count = n / y_train_classes['class'].value_counts().sort_index(axis=0)
 
-    class_weight = {}
-    for idx, value in enumerate(y_train_classes_count):
-        class_weight[idx] = value
+        class_weight = {}
+        for idx, value in enumerate(y_train_classes_count):
+            class_weight[idx] = value
+    else:
+        class_weight = None
 
     # callback function to save the trained model
     graph_path = path.join(ROOT_DIR, 'saved_models', 'focal_loss', 'model.json')
@@ -157,6 +122,7 @@ def neural_network(num_feature=8, lr=0.0005, batch_size=64, epochs=1):
     print('Test loss: {} - Test accuracy: {}'.format(score[0], score[1]))
     score = wifi.evaluate(test_raw_set[0], test_raw_set[1], verbose=0)
     print('Test_raw loss: {} - Test_raw accuracy: {}'.format(score[0], score[1]))
+
     print('Selected features: {}'.format(train.columns[mask]))
 
     # record training history
@@ -179,16 +145,7 @@ def neural_network(num_feature=8, lr=0.0005, batch_size=64, epochs=1):
                       rownames=['label'], colnames=['prediction']))
 
 
-def regression():
-    # not complete yet
-    pass
-
-
-# define classification accuracy for regression
-def soft_acc(y_true, y_pred):
-
-    return K.mean(K.equal(K.round(y_true), K.round(y_pred)))
-
-
 if __name__ == '__main__':
-    neural_network(num_feature=20, lr=0.0005, batch_size=64, epochs=30)
+    neural_network(data="attenuator", ap_only=False, drop_error=False,
+                   num_feature=20, lr=0.0005, batch_size=64, epochs=30,
+                   weighted_loss=True)
--- src/model/nn_model_rahul_keras.py
+++ src/model/nn_model_rahul_keras.py
@@ -16,32 +16,62 @@ from keras.regularizers import l1_l2
 def nn_model(input_shape):
 
     X_input = Input(input_shape)
-    X = Dense(128, kernel_initializer='he_uniform', name='fc1',
-              kernel_regularizer=l1_l2(l1=0.000, l2=0.0005))(X_input)
+    X = Dense(128, kernel_initializer='he_normal', name='fc1',
+              kernel_regularizer=l1_l2(l1=0.0, l2=0.1))(X_input)
     X = BatchNormalization(name='bn1')(X)
     X = Activation('relu')(X)
-    X = Dense(128, kernel_initializer='he_uniform', name='fc2',
-              kernel_regularizer=l1_l2(l1=0.000, l2=0.0005))(X)
+    #X = Dropout(0.2)(X)
+    X = Dense(128, kernel_initializer='he_normal', name='fc2',
+              kernel_regularizer=l1_l2(l1=0.0, l2=0.1))(X)
     X = BatchNormalization(name='bn2')(X)
     X = Activation('relu')(X)
-    X = Dense(256, kernel_initializer='he_uniform', name='fc3',
-              kernel_regularizer=l1_l2(l1=0.000, l2=0.0005))(X)
+    #X = Dropout(0.2)(X)
+    X = Dense(256, kernel_initializer='he_normal', name='fc3',
+              kernel_regularizer=l1_l2(l1=0.0, l2=0.1))(X)
     X = BatchNormalization(name='bn3')(X)
     X = Activation('relu')(X)
-    X = Dense(256, kernel_initializer='he_uniform', name='fc4',
-              kernel_regularizer=l1_l2(l1=0.000, l2=0.0005))(X)
+    #X = Dropout(0.2)(X)
+    X = Dense(256, kernel_initializer='he_normal', name='fc4',
+              kernel_regularizer=l1_l2(l1=0.0, l2=0.1))(X)
     X = BatchNormalization(name='bn4')(X)
     X = Activation('relu')(X)
-    X = Dense(512, kernel_initializer='he_uniform', name='fc5',
-              kernel_regularizer=l1_l2(l1=0.000, l2=0.0005))(X)
+    #X = Dropout(0.2)(X)
+    
+    X = Dense(512, kernel_initializer='he_normal', name='fc5',
+              kernel_regularizer=l1_l2(l1=0.0, l2=0.1))(X)
     X = BatchNormalization(name='bn5')(X)
     X = Activation('relu')(X)
-    X = Dense(512, kernel_initializer='he_uniform', name='fc6',
-              kernel_regularizer=l1_l2(l1=0.000, l2=0.0005))(X)
+    #X = Dropout(0.2)(X)
+    X = Dense(512, kernel_initializer='he_normal', name='fc6',
+              kernel_regularizer=l1_l2(l1=0.0, l2=0.1))(X)
     X = BatchNormalization(name='bn6')(X)
     X = Activation('relu')(X)
-    X = Dense(4, activation='softmax', kernel_initializer='he_uniform',
-              name='output', kernel_regularizer=l1_l2(l1=0.000, l2=0.0005))(X)
+    #X = Dropout(0.2)(X)
+    '''
+    X = Dense(64, kernel_initializer='he_uniform', name='fc7',
+              kernel_regularizer=l1_l2(l1=0.000, l2=0.0000))(X)
+    X = BatchNormalization(name='bn7')(X)
+    X = Activation('relu')(X)
+    X = Dropout(0.1)(X)
+    X = Dense(64, kernel_initializer='he_uniform', name='fc8',
+              kernel_regularizer=l1_l2(l1=0.000, l2=0.0000))(X)
+    X = BatchNormalization(name='bn8')(X)
+    X = Activation('relu')(X)
+    X = Dropout(0.1)(X)
+    
+    X = Dense(128, kernel_initializer='he_uniform', name='fc9',
+              kernel_regularizer=l1_l2(l1=0.000, l2=0.0000))(X)
+    X = BatchNormalization(name='bn9')(X)
+    X = Activation('relu')(X)
+    X = Dropout(0.1)(X)
+    X = Dense(128, kernel_initializer='he_uniform', name='fc10',
+              kernel_regularizer=l1_l2(l1=0.000, l2=0.0000))(X)
+    X = BatchNormalization(name='bn10')(X)
+    X = Activation('relu')(X)
+    X = Dropout(0.1)(X)
+    '''
+    X = Dense(4, activation='softmax', kernel_initializer='he_normal',
+              name='output', kernel_regularizer=l1_l2(l1=0.0, l2=0.1))(X)
 
     model = Model(inputs=X_input, outputs=X, name='wifi')
 
@@ -55,27 +85,27 @@ def nn_model(input_shape):
     X = Dense(128, kernel_initializer='he_uniform', name='fc1',
               kernel_regularizer=l1_l2(l1=0.000, l2=0.0001))(X_input)
     X = BatchNormalization(name='bn1')(X)
-    X = LeakyReLU()(X)
+    X = LeakyReLU(alpha=0.1)(X)
     X = Dense(128, kernel_initializer='he_uniform', name='fc2',
               kernel_regularizer=l1_l2(l1=0.000, l2=0.0001))(X)
     X = BatchNormalization(name='bn2')(X)
-    X = LeakyReLU()(X)
+    X = LeakyReLU(alpha=0.1)(X)
     X = Dense(256, kernel_initializer='he_uniform', name='fc3',
               kernel_regularizer=l1_l2(l1=0.000, l2=0.0001))(X)
     X = BatchNormalization(name='bn3')(X)
-    X = LeakyReLU()(X)
+    X = LeakyReLU(alpha=0.1)(X)
     X = Dense(256, kernel_initializer='he_uniform', name='fc4',
               kernel_regularizer=l1_l2(l1=0.000, l2=0.0001))(X)
     X = BatchNormalization(name='bn4')(X)
-    X = LeakyReLU()(X)
+    X = LeakyReLU(alpha=0.1)(X)
     X = Dense(512, kernel_initializer='he_uniform', name='fc5',
               kernel_regularizer=l1_l2(l1=0.000, l2=0.0001))(X)
     X = BatchNormalization(name='bn5')(X)
-    X = LeakyReLU()(X)
+    X = LeakyReLU(alpha=0.1)(X)
     X = Dense(512, kernel_initializer='he_uniform', name='fc6',
               kernel_regularizer=l1_l2(l1=0.000, l2=0.0001))(X)
     X = BatchNormalization(name='bn6')(X)
-    X = LeakyReLU()(X)
+    X = LeakyReLU(alpha=0.1)(X)
     X = Dense(4, activation='softmax', kernel_initializer='he_uniform',
               name='output', kernel_regularizer=l1_l2(l1=0.000, l2=0.0001))(X)
 
